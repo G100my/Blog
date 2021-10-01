5b3a786385d0f930dc9a71187ffbe1c6efd60b80
G100my <G100my.lan229@gmail.com>
2021-10-02 01:02:15 +0800

    feat: add murmur container

--- docs/.vuepress/pluginConfig.ts
+++ docs/.vuepress/pluginConfig.ts
@@ -1,20 +1,24 @@
-import { ContainerPluginOptions } from '@vuepress/plugin-container/lib/node/containerPlugin';
-import { PluginConfig, PluginOptions } from 'vuepress-vite';
+import { ContainerPluginOptions } from '@vuepress/plugin-container/lib/node/containerPlugin'
+import { PluginConfig, PluginOptions } from 'vuepress-vite'
 
 const pluginsConfig: PluginConfig<PluginOptions>[] = [
   [
     '@vuepress/container',
     {
       type: 'info',
-      before: (title: string): string =>
-        `<div class="custom-container tip">${title ? `<p class="custom-container-title">${title}</p>` : ''}\n`,
-      after: (): string => '</div>\n'
+      before: (title: string): string => `<div class="custom-container tip"><p class="custom-container-title">${title ? `title` : 'Info'}</p>\n`,
+      after: (): string => '</div>\n',
     } as ContainerPluginOptions,
   ],
   [
-    '@vuepress/plugin-palette',
-    { preset: 'sass' },
+    '@vuepress/container',
+    {
+      type: 'murmur',
+      before: (title: string): string => `<div class="custom-container murmur"><p class="custom-container-title">${title ? title : 'Just murmur ...'}</p>\n`,
+      after: (): string => '</div>\n',
+    } as ContainerPluginOptions,
   ],
-];
+  ['@vuepress/plugin-palette', { preset: 'sass' }],
+]
 
-export default pluginsConfig;
+export default pluginsConfig
