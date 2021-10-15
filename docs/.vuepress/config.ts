1b867ab7f033d9ac5b8bb2b4d5c0403ec8f61d58
G100 <G100my.lan229@gmail.com>
2021-10-16 01:33:18 +0800

    ! set postcss.config in vuepress config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,5 @@
 import { defineUserConfig } from 'vuepress-vite'
-import type { DefaultThemeOptions } from 'vuepress-vite'
+import type { DefaultThemeOptions, ViteBundlerOptions } from 'vuepress-vite'
 import { getNavConfig, getSidebarConfig } from '../../utils/getConfig'
 import pluginsConfig from './pluginConfig'
 
@@ -18,5 +18,20 @@ export default defineUserConfig<DefaultThemeOptions>({
     sidebarDepth: 0,
     repo: 'https://github.com/G100my',
   },
+
   plugins: pluginsConfig,
+
+  bundlerConfig: {
+    viteOptions: {
+      css: {
+        postcss: {
+          plugins: [
+            require('tailwindcss'),
+            require('autoprefixer')
+          ]
+        }
+      },
+
+    }
+  } as ViteBundlerOptions
 })
