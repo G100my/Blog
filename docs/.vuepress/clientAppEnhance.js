c3242dad0d7bcadd2bd9c508f01e1ac8cd99fd79
G100 <G100my.lan229@gmail.com>
2021-10-16 01:37:24 +0800

    ! inject tailwind in clientAppEnhance.js

--- docs/.vuepress/clientAppEnhance.js
+++ docs/.vuepress/clientAppEnhance.js
@@ -1,6 +1,8 @@
 import { defineClientAppEnhance } from '@vuepress/client'
 import Home from './Home.vue'
 
+import 'tailwindcss/tailwind.css'
+
 export default defineClientAppEnhance(({ app }) => {
   app.component('Home', Home)
 })
