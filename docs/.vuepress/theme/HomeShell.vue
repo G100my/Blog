d08ff65c6997e884b18e4c9c9d6441069d56f65d
G100 <G100my.lan229@gmail.com>
2021-10-17 03:11:54 +0800

    use global component - Home

---
+++ docs/.vuepress/theme/HomeShell.vue
@@ -0,0 +1,11 @@
+<template>
+  <Layout>
+    <template #page>
+      <Home />
+    </template>
+  </Layout>
+</template>
+<script setup>
+import Layout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
+import Home from '../../../src/views/Home.vue'
+</script>
