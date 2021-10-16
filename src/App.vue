115384d8e7bbf71b48d8608c98df8bb59f160461
G100 <G100my.lan229@gmail.com>
2021-10-16 18:17:59 +0800

    init theme develop set

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  <h1>For theme develop</h1>
+</template>
