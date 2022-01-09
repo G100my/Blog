91a11ce9b72f49e675bae59b74e5eae33fe4c0f0
G100 <G100my.lan229@gmail.com>
2022-01-10 04:47:32 +0800

    feat: vite.config - add resolve.alias

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,11 @@ export default defineConfig({
     postcss: {
       plugins: [require('tailwindcss'), require('postcss-nested'), require('autoprefixer')],
     },
+  },
+  resolve: {
+    alias: [
+      { find: 'src', replacement: path.resolve(__dirname, './src') },
+      { find: 'docs', replacement: path.resolve(__dirname, './docs') },
+    ],
   },
 })
