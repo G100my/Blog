c56c6263cb6662d33b5162588c7afcc2b9b65fd9
G100 <G100my.lan229@gmail.com>
2022-01-12 03:01:41 +0800

    (Vue) init vue - 01

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,16 +9,25 @@ export default defineUserConfig<DefaultThemeOptions>({
   title: 'G100 印象派 前端 筆記',
   description: 'Just playing around',
   head: [
-    ['meta', { name: "google-site-verification", content: "tf18HhmYfg1OqUkm85gsOQgzEf3eCml_yx2uBLfKGrc" }],
-    ['link', { rel: 'icon', type: "image/x-icon", href: 'favicon.ico?v=2' }],
+    ['meta', { name: 'google-site-verification', content: 'tf18HhmYfg1OqUkm85gsOQgzEf3eCml_yx2uBLfKGrc' }],
+    ['link', { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico?v=2' }],
   ],
   base: '/',
 
   themeConfig: {
     logo: '/g100-logo.png',
     logoDark: '/g100-logo-dark.png',
-    navbar: [...makeNavbarRoute('vuepress', 'Vuepress'), ...makeNavbarRoute('frontend-note', 'Front-end 筆記'), ...makeNavbarRoute('sideProject', 'Side Project')],
-    sidebar: { ...makeSidebarRoute('vuepress', 'Vuepress'), ...makeSidebarRoute('frontend-note', 'Front-end 筆記') },
+    navbar: [
+      ...makeNavbarRoute('vuepress', 'Vuepress'),
+      ...makeNavbarRoute('frontend-note', 'Front-end 筆記'),
+      ...makeNavbarRoute('sideProject', 'Side Project'),
+      ...makeNavbarRoute('vue', 'Vue'),
+    ],
+    sidebar: {
+      ...makeSidebarRoute('vuepress', 'Vuepress'),
+      ...makeSidebarRoute('frontend-note', 'Front-end 筆記'),
+      ...makeSidebarRoute('vue', 'Vue'),
+    },
     sidebarDepth: 0,
     repo: 'https://github.com/G100my/G100my.github.io/',
   },
@@ -30,14 +39,9 @@ export default defineUserConfig<DefaultThemeOptions>({
       plugins: [svgLoader()],
       css: {
         postcss: {
-          plugins: [
-            require('tailwindcss'),
-            require('postcss-nested'),
-            require('autoprefixer')
-          ]
-        }
+          plugins: [require('tailwindcss'), require('postcss-nested'), require('autoprefixer')],
+        },
       },
-
-    }
-  } as ViteBundlerOptions
+    },
+  } as ViteBundlerOptions,
 })
