12805f321e3d2035594bfb9d890234853be074c3
G100 <g100my.lan229@gmail.com>
2022-02-08 01:15:03 +0800

    add vuepress style in dev mode env

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
+<script setup>
+function switchDarkMode() {
+  const htmlClasses = document.documentElement.classList
+  console.log(htmlClasses)
+  if (htmlClasses.contains('dark')) {
+    htmlClasses.remove('dark')
+  } else {
+    htmlClasses.add('dark')
+  }
+}
+</script>
 <template>
+  <button class="fixed right-0 left-0 mx-auto z-10" @click="switchDarkMode">dark mode?</button>
   <router-view />
 </template>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import { router } from './router'
 
 import 'tailwindcss/tailwind.css'
+import '@vuepress/theme-default/lib/client/styles/normalize.scss'
+import 'docs/.vuepress/styles/index.scss'
 
 createApp(App).use(router).mount('#app')
