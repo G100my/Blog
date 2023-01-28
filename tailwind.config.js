ee81f3afa8036bdcefc6fe4f79cefa3825b32d8d
G100 <g100my.lan229@gmail.com>
2023-01-28 11:07:25 +0800

    core: update tailwind breakchange

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./docs/.vuepress/*.vue', './docs/**/*.vue', './src/**/*.vue'],
-  darkMode: false, // or 'media' or 'class'
-  corePlugins: {
-    preflight: false,
-    float: false,
-    accessibility: false,
-    clear: false,
-    tableLayout: false,
-  },
+  content: ['docs/**/*.vue'],
   theme: {
     extend: {},
   },
