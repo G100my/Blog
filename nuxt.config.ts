f2b3a5b89d9434a123ef93c02139cbdd95b49114
G100 <g100mylan229@gmail.com>
2023-08-27 21:59:08 +0800

    google font icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
+  app: {
+    head: {
+      link: [
+        {
+          // google font icon
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0',
+        },
+      ],
+    },
+  },
   i18n: {
     // Link localizing settings
     locales: ['en', 'zh'],
